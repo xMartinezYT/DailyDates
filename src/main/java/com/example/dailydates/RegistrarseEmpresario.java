0938a43dc8ba84c0e26dd046d5051cacf6a2ace0
[Gonzalo] <[paugonzalo108@gmail.com]>
2024-05-15 11:49:48 +0200

    Nueva interfaz para el registro del empresario

--- src/main/java/com/example/dailydates/RegistrarseEmpresario.java
+++ src/main/java/com/example/dailydates/RegistrarseEmpresario.java
@@ -1,5 +1,9 @@
 package com.example.dailydates;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class RegistrarseEmpresario
@@ -7,7 +11,26 @@ public class RegistrarseEmpresario
 
     @javafx.fxml.FXML
     private AnchorPane anchorPaneMain;
+    @javafx.fxml.FXML
+    private Label introducirDatosLabel;
+    @javafx.fxml.FXML
+    private TextField nombreTextField;
+    @javafx.fxml.FXML
+    private TextField contraseñaTextField;
+    @javafx.fxml.FXML
+    private TextField telefonoTextField;
+    @javafx.fxml.FXML
+    private TextField apellidosTextField;
+    @javafx.fxml.FXML
+    private TextField gmailTextField;
+    @javafx.fxml.FXML
+    private Button crearButton;
 
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void crearButtonOnAction(ActionEvent actionEvent) {
+    }
+}
