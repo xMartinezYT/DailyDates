3109c9264a73370f8fc57aef9d3d947e601a9693
[Gonzalo] <[paugonzalo108@gmail.com]>
2024-05-15 14:02:09 +0200

    Nueva interfaz para el registro del empresario

---
+++ src/main/java/com/example/dailydates/PaginaPrincipalUsuario.java
@@ -0,0 +1,32 @@
+package com.example.dailydates;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class PaginaPrincipalUsuario
+{
+    @javafx.fxml.FXML
+    private Label centroDeSaludLabel;
+    @javafx.fxml.FXML
+    private Pane fruteriaPane;
+    @javafx.fxml.FXML
+    private Pane centroDeSaludPane;
+    @javafx.fxml.FXML
+    private Label categoriaLabel;
+    @javafx.fxml.FXML
+    private ImageView fruteriaImage;
+    @javafx.fxml.FXML
+    private Pane restaurantesPane;
+    @javafx.fxml.FXML
+    private Label restaurantesLabel;
+    @javafx.fxml.FXML
+    private Label fruteriasLabel;
+    @javafx.fxml.FXML
+    private ImageView centroDeSaludImage;
+    @javafx.fxml.FXML
+    private ImageView restaurantesImage;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
