e3019c7d9c74e8d587e292f9e45e822b45c4bc9c
xuquermrtinez05 <jamaju@xuqueralumnat.es>
2024-05-16 12:56:04 +0200

    Cambio en la interfaz -> nuevo logo  y nombre de las ventanas

--- src/main/java/com/example/dailydates/HelloApplication.java
+++ src/main/java/com/example/dailydates/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.dailydates;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("DailyDates");
+        stage.getIcons().add(new Image("file:src/main/resources/com/example/dailydates/img/DailyDateFoto.png"));
         stage.setScene(scene);
         stage.show();
     }
