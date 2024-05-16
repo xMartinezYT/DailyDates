c657869fbe7fa0002613ec88d33c96cde8e7901d
[Gonzalo] <[paugonzalo108@gmail.com]>
2024-05-16 09:50:54 +0200

    Nueva interfaz para el menu del usuario

--- src/main/java/com/example/dailydates/PaginaPrincipalUsuario.java
+++ src/main/java/com/example/dailydates/PaginaPrincipalUsuario.java
@@ -1,8 +1,14 @@
 package com.example.dailydates;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+
+import java.io.IOException;
 
 public class PaginaPrincipalUsuario
 {
@@ -26,7 +32,54 @@ public class PaginaPrincipalUsuario
     private ImageView centroDeSaludImage;
     @javafx.fxml.FXML
     private ImageView restaurantesImage;
+    @javafx.fxml.FXML
+    private Button fruteriaButton;
+    @javafx.fxml.FXML
+    private AnchorPane anchorPaneMain;
+    @javafx.fxml.FXML
+    private Pane menuPane;
+    @javafx.fxml.FXML
+    private Button categoriaButton;
+    @javafx.fxml.FXML
+    private Button citasButton;
+    @javafx.fxml.FXML
+    private Label perfilMenuLabel;
+    @javafx.fxml.FXML
+    private Button perfilButton;
+    @javafx.fxml.FXML
+    private AnchorPane anchorPaneUsuario;
+    @javafx.fxml.FXML
+    private Label citasMenuLabel;
+    @javafx.fxml.FXML
+    private Label categoriaMenuLabel;
 
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @Deprecated
+    public void centroDeSaludButtonOnAction(ActionEvent actionEvent) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("RegistrarseEmpresario.fxml"));
+            this.anchorPaneMain.getChildren().setAll(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Deprecated
+    public void fruteriaButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void categoriaButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void citasButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void perfilButtonOnAction(ActionEvent actionEvent) {
+    }
+}
