244254b9471c41fa4f2f191b320356f6cba31c4b
[Gonzalo] <[paugonzalo108@gmail.com]>
2024-05-20 09:39:03 +0200

    interfaz de las citas creada

---
+++ src/main/java/com/example/dailydates/Citasmostrar.java
@@ -0,0 +1,20 @@
+package com.example.dailydates;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class Citasmostrar
+{
+    @javafx.fxml.FXML
+    private AnchorPane mainAnchorPane;
+    @javafx.fxml.FXML
+    private Label labelHora;
+    @javafx.fxml.FXML
+    private Label nombreNegocio;
+    @javafx.fxml.FXML
+    private Label labelFecha;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
