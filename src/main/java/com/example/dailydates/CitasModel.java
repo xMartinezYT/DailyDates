347dd66d0194e8ad94efbb7530e662b4a9dd05ab
Pablo Albendin <albendinpablo@gmail.com>
2024-05-21 11:51:28 +0200

    acabar la clase categoria model y crear citasmodel, ademas de un pequeño ajuste en usuariomdel

--- src/main/java/com/example/dailydates/CitasModel.java
+++ src/main/java/com/example/dailydates/CitasModel.java
@@ -1,39 +1,38 @@
 package com.example.dailydates;
 
-import javafx.scene.image.Image;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class CategoriaModel extends Conexion{
+public class CitasModel extends Conexion{
 
-    public ArrayList<Categoria> listar_categorias(){
-        ArrayList<Categoria> lista = new ArrayList<>();
-
+    public ArrayList<Citas> listar_citas(){
+        ArrayList<Citas> lista = new ArrayList<>();
         try {
-            String sql = "Select * from categoria";
+            String sql = "Select * from citas";
             PreparedStatement ps = this.getConexion().prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
-                Categoria g = new Categoria(rs.getInt(1),rs.getString(3),rs.getString(2));
-                lista.add(g);
+                  Citas c =  new Citas(rs.getInt(2),rs.getInt(1),rs.getDate(3).toLocalDate(),rs.getTime(5),rs.getString(4));
+                  lista.add(c);
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return lista;
     }
+    public boolean anyadir_cita(Citas c){
+        boolean resultado = false;
 
-    public boolean anyadir_categoria(Categoria c){
-            boolean resultado = false;
         try {
-            String sql = "insert into categoria values(?,?)";
+            String sql = "insert into citas values(?,?,?,?,?)";
             PreparedStatement ps = this.getConexion().prepareStatement(sql);
-            ps.setString(1,c.getDescripcion());
-            ps.setString(2,c.getNombre());
+
+            ps.setInt(1,c.getId_empresa());
+            ps.setInt(2,c.getId_usuario());
+            ps.setDate(3, Date.valueOf(c.getFecha()));
+            ps.setString(4,c.getPedido());
+            ps.setTime(5,c.getHora());
             ps.execute();
 
             resultado = true;
@@ -41,14 +40,14 @@ public class CategoriaModel extends Conexion{
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
         return resultado;
     }
-
-    public boolean eliminar_categoria(int id){
+    public boolean eliminar_cita(int id){
         boolean resultado = false;
 
         try {
-            String sql = "delete from categoria where id = ?";
+            String sql = "delete from citas where id = ?";
             PreparedStatement ps = this.getConexion().prepareStatement(sql);
             ps.setInt(1,id);
             ps.execute();
@@ -59,21 +58,21 @@ public class CategoriaModel extends Conexion{
         return resultado;
     }
 
-    public Categoria buscar_categoria(int id){
-        Categoria g = null;
+    public Citas buscar_cita(LocalDate fecha, Time hora){
+        Citas c = null;
+
         try {
-            String sql = "Select * from categoria";
+            String sql = "Select * from citas where fecha = ?, hora = ?";
             PreparedStatement ps = this.getConexion().prepareStatement(sql);
+            ps.setDate(1,Date.valueOf(fecha));
+            ps.setTime(2,hora);
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
-                g = new Categoria(rs.getInt(1),rs.getString(3),rs.getString(2));
+                c =  new Citas(rs.getInt(2),rs.getInt(1),rs.getDate(3).toLocalDate(),rs.getTime(5),rs.getString(4));
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return g;
+        return c;
     }
-
-
 }
