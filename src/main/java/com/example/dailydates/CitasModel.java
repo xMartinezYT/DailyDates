db3cc5da6318f63ef7a82d9477ed32f3f7776b68
Pablo Albendin <albendinpablo@gmail.com>
2024-05-29 09:57:10 +0200

    calendario del empresario acabado

--- src/main/java/com/example/dailydates/CitasModel.java
+++ src/main/java/com/example/dailydates/CitasModel.java
@@ -107,4 +107,20 @@ public class CitasModel extends Conexion{
         }
         return lista;
     }
+    public ArrayList<Citas> listar_citas_empresario(int id_empresario){
+        ArrayList<Citas> lista = new ArrayList<>();
+        try {
+            String sql = "Select * from citas where id_empresa in (select id_empresa from empresa where id_empresario = ?)";
+            PreparedStatement ps = this.getConexion().prepareStatement(sql);
+            ps.setInt(1,id_empresario);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Citas c =  new Citas(rs.getInt(2),rs.getInt(1),rs.getDate(3).toLocalDate(),rs.getTime(5),rs.getString(4));
+                lista.add(c);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return lista;
+    }
 }
